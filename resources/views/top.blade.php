0f2163d7155687bafabd902305e2bcdcf8ab35b8
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2017-04-06 13:34:06 +0900

    blade 分ける

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,14 +1,10 @@
-<!doctype html>
-<html lang="jp">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Instagram</title>
-</head>
-<body>
+@extends('temp/temp')
+
+@section('title','Instagram')
+
+@section('content')
+<main>
 
 
-
-</body>
-</html>
+</main>
+@endsection
