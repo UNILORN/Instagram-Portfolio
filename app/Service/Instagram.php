9ca5b2ad1863928df6c8b94d86906dd66c6d630f
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2017-05-16 12:52:05 +0900

    Instagram API途中

--- app/Service/Instagram.php
+++ app/Service/Instagram.php
@@ -4,18 +4,28 @@ namespace App\Service;
 
 class Instagram
 {
+
+    private $base_url = "https://api.instagram.com/v1/users/self/media/recent";
+    private $access_token = "";
+
     public function __construct()
     {
-
-
-
-
+        $this->access_token = env("INSTAGRAM_ID");
     }
 
     /**
      * @param array $data
+     * @see Instagram APIより画像情報
      */
-    public static function getAllColumn(array $data)
+    public function getAllColumn(array $data)
     {
+        $query = [
+            $this->access_token,
+
+        ];
+        $response = file_get_contents(
+            $this->base_url.
+            http_build_query($query)
+        );
     }
 }
