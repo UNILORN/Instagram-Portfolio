65dd3825edeb0042c7bb9c3faffbb34ba3c44f74
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2017-06-23 13:16:37 +0900

    update

--- resources/views/admin/temp/head.blade.php
+++ resources/views/admin/temp/head.blade.php
@@ -1,7 +1,12 @@
 <header>
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container">
-
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                    <li><a href="#">Link</a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
         </div>
     </nav>
 </header>
