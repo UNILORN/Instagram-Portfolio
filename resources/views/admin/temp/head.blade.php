cc636f626569d143cf774b0caedd6be1a7f59f6d
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2017-06-23 14:00:50 +0900

    update

--- resources/views/admin/temp/head.blade.php
+++ resources/views/admin/temp/head.blade.php
@@ -3,10 +3,10 @@
         <div class="container">
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                    <li><a href="#">Link</a></li>
+                    <li class="active"><a href="#">Administrator</a></li>
+                    <li><a href="#">Instagram</a></li>
                 </ul>
-            </div><!-- /.navbar-collapse -->
+            </div>
         </div>
     </nav>
 </header>
